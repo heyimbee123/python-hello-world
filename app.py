daf24b6a6a34e9f67f28f8d0532a20f1ef36d2e1
heyimbee123 <150716106+heyimbee123@users.noreply.github.com>
2025-09-16 19:15:42 +0200

    Add main function to run the app on Elastic Beanstalk

--- app.py
+++ app.py
@@ -4,3 +4,8 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Oscar en Alexander zijn de allerbeste"
+
+if __name__ == "__main__":
+    # Elastic Beanstalk sets PORT automatically
+    port = int(os.environ.get("PORT", 8080))
+    application.run(host="0.0.0.0", port=port)
